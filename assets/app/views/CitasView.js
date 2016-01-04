0157700d4fe36c6258c82736ed24b6f8e839cd3e
Juan David Gomez <judagoes@gmail.com>
2016-01-04 00:56:26 -0500

    datepicker config week

--- assets/app/views/CitasView.js
+++ assets/app/views/CitasView.js
@@ -3,27 +3,49 @@ define(['backbone','text!templates/citas/calendar.html','moment'],function(backb
 	var CitasView = Backbone.View.extend({
 		el:'#app-content',
 		events: {
-		 'click .day' : 'showAlert'
+		 //'click .day' : 'showAlert'
 
-		},
-
-		showAlert: function(){
-			alert("You clicked me");
-			return false;
 		},
 		initialize:function  () {
 			
 		},
+
 		render:function(){
 
 			this.$el.html($(calendarTemplate).html());
-			//$(".day").on('click',this.showAlert);
+			this.configCalendar();
+			this.eventsCalendar();
+		},
+
+		//Metodos personalizados
+		configCalendar: function  () {
 			$('#calendar').fullCalendar();
 			$('#calendarNav').datepicker({
 			    language: "es",
 			});
-			$('#calendarNav').datepicker()
-		    .on('changeDate', function(e) {
+			this.hoverWeekDatepicker();
+		},
+		hoverWeekDatepicker:function(){
+			/*Config Week Styles datepicker*/
+			var tr = $('body').find('.datepicker-days table tbody tr');
+		    tr.on("mouseover",function(){
+		        $(this).addClass('week');
+		    });
+		    tr.on("mouseout",function(){
+		        $(this).removeClass('week');
+		    });	
+
+		    $('body').find('.datepicker-days table tbody tr').removeClass('week-active');
+
+		    // add active class
+		    var tr = $('body').find('.datepicker-days table tbody tr td.active.day').parent();
+		    tr.addClass('week-active');
+		},
+		eventsCalendar:function(){
+			var self = this;
+			$('#calendarNav').datepicker().on('changeDate', function(e) {
+
+				self.hoverWeekDatepicker();
 		        var dateObject = e.date;
 			    var dateArray = dateObject.toLocaleString('es',{hour12:false}).split(" ");
 			    var dateString = dateArray[0];
@@ -31,7 +53,7 @@ define(['backbone','text!templates/citas/calendar.html','moment'],function(backb
 			    var lastDate =  moment(dateString, "DD/MM/YYYY").day(6).format("MM-DD-YYYY");
 			    console.log(firstDate);
 			    console.log(lastDate);
-		    });
+		    });	
 		}
 	});
 
